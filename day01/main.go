1375921ed0e9d5ed23427b6b917092c4746a7e62
Lucas Bearden <lpb@Lucass-MBP-2.localdomain>
2025-12-06 21:40:51 -0500

    day01

--- day01/main.go
+++ day01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"aoc2025/util"
 )
@@ -10,18 +11,77 @@ func part1(input string) int {
 	lines := util.Lines(input)
 	_ = lines
 
-	// Your solution here
+	lockPosition := 50
+	zc := 0
 
-	return 0
+	for _, value := range lines {
+		dir := value[0]
+		steps, err := strconv.Atoi(value[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		if dir == 'R' {
+			lockPosition += steps
+			for lockPosition > 99 {
+				lockPosition = lockPosition - 100
+			}
+		}
+
+		if dir == 'L' {
+			lockPosition -= steps
+			for lockPosition < 0 {
+				lockPosition = 100 + lockPosition
+			}
+		}
+
+		if lockPosition == 0 {
+			zc++
+		}
+	}
+
+	return zc
 }
 
 func part2(input string) int {
 	lines := util.Lines(input)
-	_ = lines
 
-	// Your solution here
+	lockPosition := 50
+	zc := 0
 
-	return 0
+	for _, value := range lines {
+		dir := value[0]
+		steps, err := strconv.Atoi(value[1:])
+		if err != nil {
+			panic(err)
+		}
+		old := lockPosition
+
+		if dir == 'R' {
+			// Going right from `old` by `steps`, we cross 0 each time we pass a multiple of 100
+			zc += (old + steps) / 100
+
+			lockPosition = (old + steps) % 100
+		}
+
+		if dir == 'L' {
+			if old == 0 {
+				// Starting from 0, we only hit it again after full rotations
+				zc += steps / 100
+			} else if steps >= old {
+				// We hit 0 once when we reach it, plus once for each additional 100 steps
+				zc += 1 + (steps-old)/100
+			}
+
+			newPos := (old - steps) % 100
+			if newPos < 0 {
+				newPos += 100
+			}
+			lockPosition = newPos
+		}
+	}
+
+	return zc
 }
 
 func main() {
--- day01/main_test.go
+++ day01/main_test.go
@@ -2,11 +2,32 @@ package main
 
 import "testing"
 
-const sample = `paste sample input here`
+const sample = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
+const sample2 = `L150
+L50`
+
+const sample3 = `L150
+R50`
+
+const sample4 = `R150
+L50`
+
+const sample5 = `R150
+R50`
 
 func TestPart1(t *testing.T) {
 	got := part1(sample)
-	want := 0 // expected answer
+	want := 3 // expected answer
 
 	if got != want {
 		t.Errorf("part1() = %v, want %v", got, want)
@@ -14,10 +35,25 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	got := part2(sample)
-	want := 0 // expected answer
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{"sample", sample, 6},
+		{"sample", sample2, 2},
+		{"sample", sample3, 2},
+		{"sample", sample4, 2},
+		{"sample", sample5, 2},
+		// Add more test cases here
+	}
 
-	if got != want {
-		t.Errorf("part2() = %v, want %v", got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := part2(tt.input)
+			if got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
